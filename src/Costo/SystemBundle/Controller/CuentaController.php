fc54b11cf55ceddc26e67e3956d0e81c52002cd3
Raziel Valle <razielvalle@gambess.com>
2012-06-21 21:15:58 +0200

    RazielValle: primera version de CRUD de cuantas y gastos finalizado

--- src/Costo/SystemBundle/Controller/CuentaController.php
+++ src/Costo/SystemBundle/Controller/CuentaController.php
@@ -7,89 +7,184 @@ use Costo\SystemBundle\Model\Cuenta;
 use Costo\SystemBundle\Model\CuentaQuery;
 use Costo\SystemBundle\Form\Type\CuentaType;
 
+/**
+ * @author Raziel Valle <razielvalle@gambess.com>
+ * Controlador de Cuentas, Metods CRUD+
+ * Este controlador controla las acciones request de la pagina web
+ */
 class CuentaController extends Controller {
 
-    public function indexAction() {
-        return $this->render('CostoSystemBundle:Cuenta:index.html.twig');
-    }
-
-    public function listAction($id) {
-        if ($id === null)
-            $cuentas = CuentaQuery::create()->find();
-        else
-            $cuentas = CuentaQuery::create()->findPk($id);
-        return $this->render('CostoSystemBundle:Cuenta:list.html.twig', array('cuentas' => $cuentas));
-    }
-
     /**
-     * Crea una nueva cuenta contable
-     * @return <type>
+     * Lanza la pagina de inicio
+     * No necesita parametros y muestra un listado de las cuentas existentes en base de datos
+     * @method GET route: "/" name="index_cuenta"
+     * @return Response view
      */
-    public function createAction() {
-        $cuenta = new cuenta();
-        $form = $this->createForm(new CuentaType(), $cuenta);
-
-        $request = $this->getRequest();
-
-        if ('POST' === $request->getMethod()) {
-            $form->bindRequest($request);
-
-            if ($form->isValid()) {
-                $cuenta->save();
-                return $this->redirect($this->generateUrl('_list'));
-            }
-        }
-        return $this->render('CostoSystemBundle:Cuenta:create.html.twig', array(
-            'form' => $form->createView(),
+    public function indexAction()
+    {
+        $cuentas = CuentaQuery::create()->orderByFechaCreacionCuenta('DESC')->find();
+        return $this->render('CostoSystemBundle:Cuenta:index.html.twig', array(
+            'cuentas' => $cuentas,
         ));
     }
 
     /**
-     * Actualiza una cuenta
+     * Se busca y muestra una cuenta
+     * Necesita el id de la cuenta a mostrar
      * @param int $id
-     * @return <type>
+     * @method GET route: "/{id}/show", name="show_cuenta"
+     * @return Response view
      */
-    public function updateAction($id) {
-
-//        \print_r(\get_class_methods($this));
-//        die ();
-//        if ($id != null)
-
-
+        public function showAction($id)
+    {
         $cuenta = CuentaQuery::create()->findPk($id);
 
         if (!$cuenta) {
-            throw $this->createNotFoundException('No se ha encontrado la ciudad solicitada');
+            throw $this->createNotFoundException('No se ha encontrado la cuenta solicitada');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('CostoSystemBundle:Cuenta:show.html.twig', array(
+            'cuenta'      => $cuenta,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Muestra el Formulario de Creación de una nueva cuenta
+     * No necesita parametros
+     * @method GET route: "/new" name="new_cuenta"
+     * @return Response view
+     */
+    public function newAction()
+    {
+        $cuenta = new Cuenta();
+        $form   = $this->createForm(new CuentaType(), $cuenta);
+
+        return $this->render('CostoSystemBundle:Cuenta:new.html.twig', array(
+            'cuenta' => $cuenta,
+            'form'   => $form->createView()
+        ));
+    }
+
+    /**
+     * Guarda una cuenta y redirije o espera validacion del formulario
+     * No necesita parametros
+     * @method GET route: "/create" name="create_cuenta"
+     * @return mixed, Si es valido se muestra el cuenta creado en caso contrario exije validacion
+     */
+    public function createAction()
+    {
+        $cuenta  = new Cuenta();
+        $request = $this->getRequest();
+        $form    = $this->createForm(new CuentaType(), $cuenta);
+        $form->bindRequest($request);
+
+        if ($form->isValid()) {
+            $cuenta->save();
+            return $this->redirect($this->generateUrl('show_cuenta', array('id' => $cuenta->getIdCuenta())));
+
+        }
+
+        return $this->render('CostoSystemBundle:Cuenta:create.html.twig', array(
+            'cuenta' => $cuenta,
+            'form'   => $form->createView()
+        ));
+    }
+
+    /**
+     * Muestra el formulario de edicion de una cuenta
+     * Necesita el id de la cuenta a editar
+     * @method GET route: "/create" name="create_cuenta"
+     * @param int $id
+     * @return mixed, renderiza el formulario de edicion o un error en caso de no encontrar la cuenta
+     */
+    public function editAction($id)
+    {
+       $cuenta = CuentaQuery::create()->findPk($id);
+
+        if (!$cuenta) {
+            throw $this->createNotFoundException('No se ha encontrado la cuenta solicitada');
+        }
+
+        $editForm = $this->createForm(new CuentaType(), $cuenta);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('CostoSystemBundle:Cuenta:edit.html.twig', array(
+            'cuenta'      => $cuenta,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Actualiza y guarda un cuenta
+     * Necesita un parametro el id del cuenta a actualizar
+     * @method GET route: "/{id}/update" name="update_cuenta"
+     * @param int $id
+     * @return mixed, muestra un error o el formulario de cuentas en caso de que no sea valido
+     */
+    public function updateAction($id)
+    {
+         $cuenta = CuentaQuery::create()->findPk($id);
+
+        if (!$cuenta) {
+            throw $this->createNotFoundException('No se ha encontrado el cuenta solicitado');
         }
 
         $editForm = $this->createForm(new CuentaType(), $cuenta);
         $deleteForm = $this->createDeleteForm($id);
 
         $request = $this->getRequest();
-        if ('POST' === $request->getMethod()) {
-            $form->bindRequest($request);
 
-            if ($form->isValid()) {
-                $cuenta->save();
-                return $this->redirect($this->generateUrl('_list'));
-            }
+        $editForm->bindRequest($request);
+
+        if ($editForm->isValid()) {
+            $cuenta->save();
+
+            return $this->redirect($this->generateUrl('edit_cuenta', array('id' => $id)));
         }
 
-        return $this->render('CostoSystemBundle:Cuenta:update.html.twig', array(
-            'cuenta' => $cuenta,
-            'edit_form' => $editForm->createView(),
+        return $this->render('CostoSystemBundle:Cuenta:edit.html.twig', array(
+            'cuenta'      => $cuenta,
+            'edit_form'   => $editForm->createView(),
             'delete_form' => $deleteForm->createView(),
         ));
     }
 
-    public function deleteAction($id) {
-        return $this->render('CostoSystemBundle:Cuenta:delete.html.twig');
+    /**
+     * Borra una Cuenta
+     * Necesita el id de la cuenta a borrar
+     * @method GET route: "/{id}/delete" name="delete_cuenta"
+     * @param int $id
+     * @return mixed
+     */
+    public function deleteAction($id)
+    {
+        $form = $this->createDeleteForm($id);
+        $request = $this->getRequest();
+
+        $form->bindRequest($request);
+
+        if ($form->isValid()) {
+            $cuenta = CuentaQuery::create()->findPk($id);
+
+            if (!$cuenta) {
+            throw $this->createNotFoundException('No se ha encontrado el cuenta solicitado');
+            }
+
+            $cuenta->delete();
+        }
+
+        return $this->redirect($this->generateUrl('index_cuenta'));
     }
 
-    private function createDeleteForm($id) {
+    private function createDeleteForm($id)
+    {
         return $this->createFormBuilder(array('id' => $id))
-                ->add('id', 'hidden')
-                ->getForm()
+            ->add('id', 'hidden')
+            ->getForm()
         ;
     }
 
