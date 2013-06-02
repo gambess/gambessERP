ee27d11f14bbe19e5baa1e3397a21ff473fcbdb5
Raziel Valle <raziel.valle@openknowledgenetwork.com>
2013-06-02 19:03:16 +0200

    RazielValle: Related all in sails

--- src/Costo/SystemBundle/Controller/VentaController.php
+++ src/Costo/SystemBundle/Controller/VentaController.php
@@ -311,6 +311,7 @@ class VentaController extends Controller {
                         'real' => $re,
                         'totaldoc' => $totaldoc,
                         'documents' => $d,
+                        'id'=> $id,
                         
                         )
                 );
